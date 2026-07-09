e677b75b4dad2262f01e336cb0a196fdf0ce062e
Benicioxp <benicio.santos.silva@escola.pr.gov.br>
2026-07-09 11:45:30 -0300

    Add files via upload

--- script7.js
+++ script7.js
@@ -1,5 +1,16 @@
 function converterFaren(){
     let celsius = Number(document.getElementById("celsius").values);
     let faren = (celsius *9/5) + 32;
-    document.getElementById("resultado").innerText = "Resultado: " + faren + "K"
+    document.getElementById("resultado").innerText = "Resultado: " + faren + "F"
+}
+
+function converterKel(){
+    let celsius = Number(document.getElementById("celsius").value);
+    let kelvin = celsius + 273.15;
+    document.getElementById("resultado").innerText = "Resultado: " + kelvin + "K"
+}
+
+function limpar(){
+    document.getElementById("celsius").value = "";
+    document.getElementById("resultado").innerText = "Resultado: ";
 }
